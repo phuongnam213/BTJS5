75120c743ff89b2671d3ccf9ca9d56d310185e86
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-03-06 14:33:10 +0700

    Filter

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,54 @@ const { ConnectionCheckOutFailedEvent } = require('mongodb');
 var router = express.Router();
 let productModel = require('../schemas/product')
 
+function buildQuery(obj){
+  console.log(obj);
+  let result = {};
+  if(obj.name){
+    result.name=new RegExp(obj.name,'i');
+  }
+  result.price = {};
+  if(obj.price){
+    if(obj.price.$gte){
+      result.price.$gte = obj.price.$gte;
+    }else{
+      result.price.$gte = 0
+    }
+    if(obj.price.$lte){
+      result.price.$lte = obj.price.$lte;
+    }else{
+      result.price.$lte = 10000;
+    }
+    
+  }
+  return result;
+}
+
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  let products = await productModel.find({});
+  
+
+  let products = await productModel.find(buildQuery(req.query));
+
   res.status(200).send({
     success:true,
     data:products
   });
+});
+router.get('/:id', async function(req, res, next) {
+  try {
+    let id = req.params.id;
+    let product = await productModel.findById(id);
+    res.status(200).send({
+      success:true,
+      data:product
+    });
+  } catch (error) {
+    res.status(404).send({
+      success:false,
+      message:"khong co id phu hop"
+    });
+  }
 });
 
 router.post('/', async function(req, res, next) {
